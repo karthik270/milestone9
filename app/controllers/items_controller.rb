9d0445ba3ab2c4eab1d6829b2f980313f0a35c4d
karthik270 <karthikauti270@gmail.com>
2020-04-10 17:52:53 +0530

    Created the menus table and added the items into the table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,18 @@ class ItemsController < ApplicationController
     def index
         render plain: Item.order(:id).map{|item| item.to_pleasant_string}.join("\n")
     end
-
+    def create
+        food_item = params[:food_item]
+        quantity = params[:quantity]
+        price = params[:price]
+        new_item = Item.create!(food_item: food_item, quantity: quantity, price: price)
+        response_text = "Hey, your new food item  is into menu with the id #{new_item.id}"
+    end
+    def show
+        id = params[:id]
+        item = Item.find(id)
+        render plain: item.to_pleasant_string
+    end
 
 
 end
